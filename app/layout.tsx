f203781f5ee742331392a941041998819025abce
Zeusine <shawon2012.sk@gmail.com>
2025-07-24 00:41:45 +0600

    tab icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import StarsCanvas from "@/components/main/StarBackground";
 import Navbar from "@/components/main/Navbar";
-import Head from "next/head";
 
 
 const geistSans = Geist({
@@ -20,7 +19,12 @@ export const metadata: Metadata = {
   title: "Shahabuddin Ahmed Khan Portfolio",
   description: "Aim high they say, so space it is then",
   icons: {
-    icon: "/logo2.png",
+    icon: [
+      { url: "/logo2.png", type: "image/png" },
+      { url: "/logo2.ico", type: "image/x-icon" },
+    ],
+    shortcut: "/logo2.png",
+    apple: "/logo2.png",
   },
 };
 
@@ -31,16 +35,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Head>
-        <link rel="icon" href="/logo2.png" type="image/png" />
-      </Head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#07010c] overflow-y-scroll overflow-x-hidden`}
       >
         <StarsCanvas></StarsCanvas>
         <Navbar></Navbar>
         {children}
-        
+
       </body>
     </html>
   );
